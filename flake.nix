e6e05df77ed9b3228061c336c7a8f729fafcc31f
h7x4 <h7x4@nani.wtf>
2024-07-10 17:08:53 +0200

    flake.nix: replace fenix with rust-overlay

--- flake.nix
+++ flake.nix
@@ -1,37 +1,43 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    fenix.url = "github:nix-community/fenix";
-    fenix.inputs.nixpkgs.follows = "nixpkgs";
+
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-
-  outputs = { self, nixpkgs, fenix }@inputs:
+  outputs = { self, nixpkgs, rust-overlay }@inputs:
   let 
     systems = [
       "x86_64-linux"
       "aarch64-linux"
+      "x86_64-darwin"
       "aarch64-darwin"
+      "armv7l-linux"
     ];
     forAllSystems = f: nixpkgs.lib.genAttrs systems (system: let
-      toolchain = fenix.packages.${system}.complete;
       pkgs = import nixpkgs {
         inherit system;
         overlays = [
-          (_: super: let pkgs = fenix.inputs.nixpkgs.legacyPackages.${system}; in fenix.overlays.default pkgs pkgs)
+          (import rust-overlay)
         ];
       };
+
+      rust-bin = rust-overlay.lib.mkRustBin { } pkgs.buildPackages;
+      toolchain = rust-bin.stable.latest.default;
     in f system pkgs toolchain);
   in {
     devShell = forAllSystems (system: pkgs: toolchain: pkgs.mkShell {
-      packages = [
-        (toolchain.withComponents [
-          "cargo" "rustc" "rustfmt" "clippy"
-        ])
-        pkgs.openssl
+      nativeBuildInputs = [
+        toolchain
         pkgs.pkg-config
       ];
-      RUST_SRC_PATH = "${toolchain.rust-src}/lib/rustlib/src/rust/library";
+
+      buildInputs = [
+        pkgs.openssl
+      ];
+
+      RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
     });
   };
 }
