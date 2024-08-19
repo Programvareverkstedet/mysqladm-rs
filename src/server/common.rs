b21aa0eece37879056a99f31c24c854bdd5d25ba
h7x4 <h7x4@nani.wtf>
2024-08-19 17:57:35 +0200

    Fix sql regex to work like ownership validation

--- src/server/common.rs
+++ src/server/common.rs
@@ -5,9 +5,9 @@ use sqlx::prelude::*;
 /// that belong to the user or any of the user's groups.
 pub fn create_user_group_matching_regex(user: &UnixUser) -> String {
     if user.groups.is_empty() {
-        format!("{}(_.+)?", user.username)
+        format!("{}_.+", user.username)
     } else {
-        format!("({}|{})(_.+)?", user.username, user.groups.join("|"))
+        format!("({}|{})_.+", user.username, user.groups.join("|"))
     }
 }
 
@@ -23,4 +23,29 @@ pub fn try_get_with_binary_fallback(
         row.try_get::<Vec<u8>, _>(column)
             .map(|v| String::from_utf8_lossy(&v).to_string())
     })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use regex::Regex;
+
+    #[test]
+    fn test_create_user_group_matching_regex() {
+        let user = UnixUser {
+            username: "user".to_owned(),
+            groups: vec!["group1".to_owned(), "group2".to_owned()],
+        };
+
+        let regex = create_user_group_matching_regex(&user);
+        let re = Regex::new(&regex).unwrap();
+
+        assert!(re.is_match("user_something"));
+        assert!(re.is_match("group1_something"));
+        assert!(re.is_match("group2_something"));
+
+        assert!(!re.is_match("other_something"));
+        assert!(!re.is_match("user"));
+        assert!(!re.is_match("usersomething"));
+    }
 }
